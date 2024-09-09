c57c03c8e1d61307913f9c9c5f3f6a94625ea2d1
Gowsick.R <130524215+Gowsickraj@users.noreply.github.com>
2024-09-08 20:59:54 -0700

    new

--- src/NAVBAR/Nav.jsx
+++ src/NAVBAR/Nav.jsx
@@ -7,6 +7,9 @@ export const Layout = () => {
                 <ul>
                     <li><Link to="/Home">Home</Link></li>
                     <li><Link to="/About">About</Link></li>
+                    <li><Link to="/register">Register</Link></li>
+                    <li><Link to="/logout">LogIn</Link></li>
+                    <li><Link to="/profile">Profile</Link></li>
                 </ul>
             </nav>
             <Outlet/>
