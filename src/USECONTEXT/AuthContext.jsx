2449aa8c4fe639322f655c496c8d0f0de353337f
Gowsick.R <130524215+Gowsickraj@users.noreply.github.com>
2024-09-08 21:02:25 -0700

    new

---
+++ src/USECONTEXT/AuthContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useEffect, useReducer, useState } from "react";
+
+export const AuthContext = createContext()
+const Authreducer = (state, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            return { user: action.playload }
+        case 'LOGOUT':
+            localStorage.removeItem('token')
+            return { user: null }
+            default:
+                return state;
+    }
+}
+const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(Authreducer, { user: null })
+    const [loading,setLoading] = useState(true)
+    useEffect(()=>{
+        let user = localStorage.getItem("token")
+            if(user!=null){
+                    dispatch({type:'LOGIN',payload: user})
+            }
+        setLoading(false);
+    },[])
+        if(!loading)
+    return (
+        <AuthContext.Provider value={{...state,dispatch}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+export default AuthContextProvider
